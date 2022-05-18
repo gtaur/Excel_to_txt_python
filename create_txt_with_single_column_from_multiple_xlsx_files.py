f10a955c453bdcf042c68a03f6f33a8b8e1b1071
gtaur <50932540+gtaur@users.noreply.github.com>
2022-05-18 14:54:39 +0200

    Update create_txt_with_single_column_from_multiple_xlsx_files.py

--- create_txt_with_single_column_from_multiple_xlsx_files.py
+++ create_txt_with_single_column_from_multiple_xlsx_files.py
@@ -1,87 +1,84 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 17 09:54:23 2022
+Created on Wed May 18 10:53:06 2022
 
 @author: gtaur
 """
 
 
-# importing pandas module
-import os
+# import necessary libraries
 import pandas as pd
+import os
+import glob
 
 
+#make dir
+userfolder = "HF469NN"
+folder_files = "C:/Users/"+ userfolder +"/OneDrive - EY/Documents/Py_file_folder/"
 
-#make directory variable
-userfolder = "" #your userfolder
-deskt = "C:/Users/"+ userfolder +"/OneDrive - /Desktop/" #your desktop folder 
+# use glob to get all the csv files
+# in the folder
+path = folder_files
+xl_files = glob.glob(os.path.join(path, "*.xlsx"))
 
-#create files vars
-file1 = deskt+"xxx.xlsx" #filename1
+lista_df = []
 
-file2 = deskt+"xxx.xlsx" #filename2
+# loop over the list of xl files
+for f in xl_files:
 
-file3 = deskt+"xxx.xlsx" #filename3
+	# read the csv file
+	df_temp = pd.read_excel(f)
+    
+	lista_df.append(df_temp)	
+	# print the content
+	print('Content:')
+	print(df_temp)  
+	
+    
+df_tot = pd.concat(lista_df) #concat df
 
-file4 = deskt+"xxx.xlsx" #filename4
+df = df_tot
 
- 
-# import from excl
-
-
-df1 = pd.read_excel(file1,index_col=None)
-
-df2 = pd.read_excel(file2,index_col=None)
-
-df3 = pd.read_excel(file3,index_col=None)
-
-df4 = pd.read_excel(file4,index_col=None)
-
-
-#unisci i df
-
-df = df1.append(df2,ignore_index = True)
-
-df = df.append(df3,ignore_index = True)
-
-df = df.append(df4,ignore_index = True)
-
-#select I DONE
-
-select = df[df['Stato'] == 'Done'] #change cases
+#select column
+	
+select = df[df['Stato'] == 'Done']
 
 select2 = select.copy()
+
+
 
 
 # dropping ALL duplicate values
 select2.drop_duplicates(subset ="Descrizione", keep = False, inplace = True)
  
 
-#pass the column to a list var
+#pass col to list
 row_task = select2['Descrizione'].tolist()
 
-#add prefix you want
+#add prefix
 
 stringa = '- '
 row_task2 = [stringa + x   for x in row_task]
 
-#print in console to check
+#print in console
 
 for row in row_task2:
     print(row)
     
     
-#print to non existing txt
+#print to new txt
 
-with open(deskt + "report202204.txt","w+") as f:
+with open(path + "report202204.txt","w+") as f:
         for row in row_task2:
             f.write("%s\n" % row)
     
 
 
 
+
+
 '''
-some recap
+some reminders
 
 w  write mode
 r  read mode
@@ -90,6 +87,8 @@ a  append mode
 w+  create file if it doesn't exist and open it in write mode
 r+  open for reading and writing. Does not create file.
 a+  create file if it doesn't exist and open it in append mode
+
+
+
+
 '''
-
-
